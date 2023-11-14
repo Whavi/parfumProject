c0568c24d7a74be15596b5282f77c618308a4cbb
B.Fethi <54514676+Whavi@users.noreply.github.com>
2023-11-14 10:28:54 +0100

    Add files via upload

---
+++ Projet perso/JS/main.js
@@ -0,0 +1,10 @@
+// Initialise le carrousel avec la bibliothèque Slick
+$(document).ready(function(){
+    $('.carousel-container').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+  
